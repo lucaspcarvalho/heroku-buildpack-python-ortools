8f085f19d3281d4c0a26e31dfee00bd22ae2ff26
Lucas <carvalho.lucasp@gmail.com>
2018-03-22 11:22:22 -0600

    changed version

--- vendor/ortools/setup.py
+++ vendor/ortools/setup.py
@@ -11,8 +11,8 @@ def read(fname):
 
 setup(
     name='ortools_examples',
-    version='6.7.4957',
-    install_requires = ['ortools==6.7.4957'],
+    version='5.0.3919',
+    install_requires = ['ortools==5.0.3919'],
     license='Apache 2.0',
     author = 'Google Inc',
     author_email = 'lperron@google.com',
